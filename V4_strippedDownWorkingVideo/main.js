12814fb65a63cd081f32c1807091e60b6cc523f0
Eve Weinberg <evejweinberg@gmail.com>
2016-03-24 13:30:37 -0400

    light inside

--- V4_strippedDownWorkingVideo/main.js
+++ V4_strippedDownWorkingVideo/main.js
@@ -7,12 +7,23 @@ var container, scene, camera, renderer, controls, alon;
 //GLOBAL VARS FOR CONTROL AND GUI
 // var keyboard = new THREEx.KeyboardState();
 var gui;
+var radExpand = 400;
 var camStartZ = 2000;
 var camZMin = -2500;
 var camZMax = 2500;
-  var camSpeed = -1;
-  var cubeCamera;
-  var zRotOn = false;
+var camSpeed = -1;
+var cubeCamera;
+var zRotValue = 0
+var zRotOn = false;
+var r = 255;
+var g = 0;
+var b = 0;
+var gmapped = 0;
+
+var bmapped = 0;
+var rmapped = 0;
+var color;
+var frontLight;
 
 //------- 3D MODEL CONTROL ----------
 
@@ -24,7 +35,7 @@ var angle = 0.001;
 var modelScale = 1;
 var particles, geometry, materials = [],
     parameters, i, h, color, size;
-    var keyboard = new THREEx.KeyboardState();
+var keyboard = new THREEx.KeyboardState();
 
 
 
@@ -114,6 +125,8 @@ function init() {
     addCubeGrid()
     addGround();
     Particles();
+    // glowBall();
+    GlowingParticles()
 
     //-------------------------------   
 
@@ -132,7 +145,7 @@ function init() {
     //try taking this out
     camera.lookAt(movieScreen.position);
 
- 
+
 
     var onProgress = function(xhr) {
         if (xhr.lengthComputable) {
@@ -162,9 +175,9 @@ function init() {
 
         //reset rotation to world
 
-        object.rotateOnAxis(new THREE.Vector3(0, 0, 0), 0.0);
-        object.rotateOnAxis(new THREE.Vector3(0, 0, 0), 0);
-        object.rotateOnAxis(new THREE.Vector3(0,0,1), 0);
+        object.rotateOnAxis(new THREE.Vector3(1, 1, 1), zRotValue);
+        // object.rotateOnAxis(new THREE.Vector3(0, 0, 0), 0);
+        // object.rotateOnAxis(new THREE.Vector3(0, 0, 1), 20);
         scene.add(object);
 
         //------ADD GUI-------
@@ -223,7 +236,7 @@ object.rotateOnAxis(new THREE.Vector3(0,1,0), value);
         videoShow.onChange(function(value) {
             movieScreen.visible = value;
         });
-       
+
 
         //Video Rotate Z 
         var zRotV = f2.add(sliders, 'rotateScreenZ', 0, 360).step(1);
@@ -277,7 +290,7 @@ object.rotateOnAxis(new THREE.Vector3(0,1,0), value);
         //Model Offset z Position
         var zPosM = f2.add(sliders, 'zPosModel', 0, 100).step(1);
         zPosM.onChange(function(value) {
-            console.log(value);
+            // console.log(value);
             object.position.z = value;
         });
 
@@ -335,50 +348,68 @@ function animate() {
 
 // ---------- UPDATE-------------------
 function update() {
+    radExpand++
+    rmapped++
+    gmapped++
+    bmapped++
+    rmapped = rmapped + .1
+
+    lightColA = "rgb(" + rmapped + "," + gmapped + "," + bmapped + ")";
+    // console.log(lightColA)
+    // color.addColors(1,1,1)
+
+
+    var time = Date.now() * 0.0000001;
+
+    for (i = 0; i < scene.children.length; i++) {
+
+        var object = scene.children[i];
+
+        if (object instanceof THREE.Points) {
+
+            object.rotation.y = time * (i < 4 ? i + 1 : -(i + 1));
+
+        }
+
+    }
+    //color changing
+    for (i = 0; i < materials.length; i++) {
+
+        color = parameters[i][0];
+
+        h = (360 * (color[0] + (time * 20)) % 360) / 360;
+        materials[i].color.setHSL(h, color[1], color[2]);
+
+    }
 
 
 
-var time = Date.now() * 0.0000001;
+    if (zRotOn) {
+        zRotValue = .003
+        object.rotateOnAxis(new THREE.Vector3(0, 0, 1), zRotValue);
 
-    for ( i = 0; i < scene.children.length; i ++ ) {
-
-                    var object = scene.children[ i ];
-
-                    if ( object instanceof THREE.Points ) {
-
-                        object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-
-                    }
-
-                }
-//color changing
-                for ( i = 0; i < materials.length; i ++ ) {
-
-                    color = parameters[i][0];
-
-                    h = ( 360 * ( color[0] + (time*20) ) % 360 ) / 360;
-                    materials[i].color.setHSL( h, color[1], color[2] );
-
-                }
-
-
-
-
+        console.log('ON' + zRotValue)
+    } else {
+        zRotValue = 0
+        object.rotateOnAxis(new THREE.Vector3(0, 0, 1), zRotValue);
+        console.log('OFF' + zRotValue)
+    }
 
 
 
     camera.position.z += camSpeed;
-        // console.log(camera.position.z)
-        if (camera.position.z > camZMax || camera.position.z < camZMin) {
-            switchCamera();
-        }
-    // if (keyboard.pressed("v")) // pause
-    //     printScreen();
-    // if (keyboard.pressed("r")) // resume
-    //     video.play();
+    // console.log(camera.position.z)
+    if (camera.position.z > camZMax || camera.position.z < camZMin) {
+        switchCamera();
+    }
+
 
     if (keyboard.pressed("s")) // resume
         zRotOn = true;
+
+    if (keyboard.pressed("d")) // resume
+        zRotOn = false;
+    // console.log(zRotOn)
 
     // if (keyboard.pressed("s")) // resume
     //     movieScreen.visible = false;
@@ -390,18 +421,17 @@ var time = Date.now() * 0.0000001;
 }
 
 // ---------- RENDER -------------------
+function rotateornot() {
+    if (zRotOn == true) {
+        zRotOn = false
+    } else if (zRotOn == false) {
+        zRotOn = true;
+    }
+}
 
 function render() {
 
-    if (zRotOn){
-        // zRotM.onChange(function(value) {
-        //     // console.log(value);
-        //     value = map_range(value, 0, 360, 0, Math.PI * 2);
-        //     object.rotation.z = value;
-        // });
 
-
-    }
 
 
     if (video.readyState === video.HAVE_ENOUGH_DATA)
@@ -436,31 +466,41 @@ function printScreen() {
 
 
 function AddLights() {
+    color = new THREE.Color("hsl(200, 100%, 50%)");
+    var lightColA = "rgb(" + r + "," + g + "," + b + ")";
     var ambient = new THREE.SpotLight(0x101030);
     ambient.position.set(0, 0, 4000)
-        scene.add(ambient);
+    scene.add(ambient);
 
-    var directionalLight = new THREE.DirectionalLight(0xffeedd, 1);
 
-    directionalLight.position.set(0, 0, 1001);
-    directionalLight.castShadow = true;
-    directionalLight.shadowDarkness = 1;
-    directionalLight.shadowMapSoft = true;
-    // scene.add(directionalLight);
 
-    var directionalLightInside = new THREE.DirectionalLight(0xffeedd, 1);
-
-    directionalLightInside.position.set(0, 0, 6001);
+    var directionalLightInside = new THREE.DirectionalLight(lightColA, 1);
+    directionalLightInside.position.set(0, 0, -3000);
     directionalLightInside.castShadow = true;
     directionalLightInside.shadowDarkness = 1;
     directionalLightInside.shadowMapSoft = true;
     scene.add(directionalLightInside);
 
-    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-                hemiLight.color.setHSL( 0.6, 1, 0.6 );
-                hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-                hemiLight.position.set( 0, 500, 0 );
-                scene.add( hemiLight );
+
+     var insidelight = new THREE.PointLight(0xffffff, 1,0,.5);
+    insidelight.position.set(0, 0,-1200);
+    insidelight.castShadow = true;
+    insidelight.shadowDarkness = 1;
+    insidelight.shadowMapSoft = true;
+    scene.add(insidelight);
+
+    frontLight = new THREE.DirectionalLight(color, 1);
+    frontLight.position.set(0, 0, 3000);
+    frontLight.castShadow = true;
+    frontLight.shadowDarkness = 1;
+    frontLight.shadowMapSoft = true;
+    scene.add(frontLight);
+
+    hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 500, 0);
+    scene.add(hemiLight);
 }
 
 
@@ -484,9 +524,9 @@ function BuildSkeleton() {
     camera.lookAt(scene.position);
 
 
-        cubeCamera = new THREE.CubeCamera( 1, 1000, 256 );
-                // cubeCamera.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
-                scene.add( cubeCamera );
+    cubeCamera = new THREE.CubeCamera(1, 1000, 256);
+    // cubeCamera.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
+    scene.add(cubeCamera);
 
     // RENDERER
     if (Detector.webgl)
@@ -504,123 +544,201 @@ function BuildSkeleton() {
 }
 
 
+function glowBall() {
+    var geometry = new THREE.SphereGeometry(0, 0, 0);
+    var material = new THREE.MeshLambertMaterial({ color: 0x000088 });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(340, 30, 30);
+    scene.add(mesh);
 
-    //setTimeout(generateGIF,1000);
-    function addCubeGrid() {
-        var cubesize = 300
-        var geo = new THREE.BoxGeometry(cubesize, cubesize, cubesize);
-        var mat = new THREE.MeshLambertMaterial({
-            color: 0xffff00
-        });
-        cube = new THREE.Mesh(geo, mat);
-        cube.position.x = -10;
-        // scene.add(cube);
+    // SUPER SIMPLE GLOW EFFECT
+    // use sprite because it appears the same from all angles
+    var spriteMaterial = new THREE.SpriteMaterial({
+        map: new THREE.ImageUtils.loadTexture('../textures/glow.png'),
+        useScreenCoordinates: false,
+        color: 0x0000ff,
+        transparent: true,
+        blending: THREE.AdditiveBlending
+    });
+    var sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(900, 900, 40.0);
+    mesh.add(sprite); // this centers the glow at the mesh
+}
 
 
 
-        for (var i = 0; i < 20; i++) {
-            for (var j = 0; j < 30; j++) {
-                mat = new THREE.MeshLambertMaterial({
-                    color: 0xFF748C
-                }); // random colors!
-                var mesh = new THREE.Mesh(geo, mat);
-                mesh.position.set(-11500 + (2100 * i), -5000 + (j * 1270), (70 + j * 40))
-                scene.add(mesh);
-            }
+//setTimeout(generateGIF,1000);
+function addCubeGrid() {
+    var cubesize = 300
+    var geo = new THREE.BoxGeometry(cubesize, cubesize, cubesize);
+    var mat = new THREE.MeshLambertMaterial({
+        color: 0xffff00
+    });
+    cube = new THREE.Mesh(geo, mat);
+    cube.position.x = -10;
+    // scene.add(cube);
+
+
+
+    for (var i = 0; i < 20; i++) {
+        for (var j = 0; j < 30; j++) {
+            mat = new THREE.MeshLambertMaterial({
+                color: 0xFF748C
+            }); // random colors!
+            var mesh = new THREE.Mesh(geo, mat);
+            mesh.position.set(-11500 + (2100 * i), -5000 + (j * 1270), (70 + j * 40))
+            scene.add(mesh);
         }
     }
+}
 
 
 
-    function addGround(){
-        // var imgTexture = new THREE.TextureLoader().load( "../textures/the.png" );
-        //         imgTexture.wrapS = imgTexture.wrapT = THREE.RepeatWrapping;
-        //         imgTexture.anisotropy = 16;
-        //         imgTexture = null;
+function addGround() {
+    // var imgTexture = new THREE.TextureLoader().load( "../textures/the.png" );
+    //         imgTexture.wrapS = imgTexture.wrapT = THREE.RepeatWrapping;
+    //         imgTexture.anisotropy = 16;
+    //         imgTexture = null;
 
-        // var material = new THREE.MeshPhongMaterial( { map: imgTexture, bumpMap: imgTexture, bumpScale: bumpScale, color: diffuseColor, specular: specularColor, reflectivity: reflectivity, shininess: specularShininess, shading: THREE.SmoothShading, envMap: localReflectionCube  } )
-
-
-
-var textureLoader = new THREE.TextureLoader();
-
-textureLoader.load( '../textures/metalPurp2.png', function ( texture ) {
+    // var material = new THREE.MeshPhongMaterial( { map: imgTexture, bumpMap: imgTexture, bumpScale: bumpScale, color: diffuseColor, specular: specularColor, reflectivity: reflectivity, shininess: specularShininess, shading: THREE.SmoothShading, envMap: localReflectionCube  } )
 
 
-    // var groundMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x050505 } );
-    //     var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
-    //             var groundMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x050505 } );
-    //             groundMat.color.setHSL( 0.095, 1, 0.75 );
-    //             var ground = new THREE.Mesh( groundGeo, groundMat );
-    //             ground.rotation.x = 0;
-    //             ground.position.y = -33;
-    //             scene.add( ground );
-    //             ground.receiveShadow = true;
 
-                texture.mapping = THREE.UVMapping;
+    var textureLoader = new THREE.TextureLoader();
 
-               var mesh = new THREE.Mesh( new THREE.SphereGeometry( 7500, 60, 40 ), new THREE.MeshLambertMaterial( { map: texture } ) );
-                mesh.scale.x = -1;
-                mesh.recieveShadow = true;
-                scene.add( mesh );
-                var material = new THREE.MeshBasicMaterial( { envMap: cubeCamera.renderTarget } );
+    textureLoader.load('../textures/metalPurp2.png', function(texture) {
 
-              
 
-            } );
+
+        texture.mapping = THREE.UVMapping;
+
+        var mesh = new THREE.Mesh(new THREE.SphereGeometry(7500, 60, 40), new THREE.MeshLambertMaterial({ map: texture }));
+        mesh.scale.x = -1;
+        mesh.recieveShadow = true;
+        scene.add(mesh);
+        var material = new THREE.MeshBasicMaterial({ envMap: cubeCamera.renderTarget });
+
+
+
+    });
+
+}
+
+
+function switchCamera() {
+    camSpeed = -camSpeed;
+    // console.log(camSpeed)
+}
+
+
+function Particles() {
+    geometry = new THREE.Geometry();
+
+    for (i = 0; i < 19000; i++) {
+
+        var vertex = new THREE.Vector3();
+        vertex.x = Math.random() * 19000 - 6000;
+        vertex.y = Math.random() * 8000 - 1000;
+        vertex.z = Math.random() * 17000 - 4000;
+
+        geometry.vertices.push(vertex);
+
+    }
+    //colors
+    parameters = [
+        [
+            [1, .4, .6], 5
+        ],
+        [
+            [1, .3, 0.4], 14
+        ],
+        [
+            [.8, .2, 0.5], 13
+        ],
+        [
+            [.5, .7, 0.3], 16
+        ],
+        [
+            [1, .5, 0.9], 11
+        ]
+    ];
+
+    for (i = 0; i < parameters.length; i++) {
+
+        color = parameters[i][0];
+        size = parameters[i][1];
+
+        materials[i] = new THREE.PointsMaterial({ size: size });
+
+        particles = new THREE.Points(geometry, materials[i]);
+
+        particles.rotation.x = Math.random() * 6;
+        particles.rotation.y = Math.random() * 6;
+        particles.rotation.z = Math.random() * 6;
+
+        scene.add(particles);
 
     }
 
 
-      function switchCamera() {
-        camSpeed = -camSpeed;
-        // console.log(camSpeed)
+
+}
+
+
+
+function GlowingParticles() {
+    var particleTexture = THREE.ImageUtils.loadTexture('../textures/glow.png');
+
+    particleGroup = new THREE.Object3D();
+    particleAttributes = { startSize: [], startPosition: [], randomness: [] };
+
+    var totalParticles = 200;
+    var radiusRange = radExpand;
+    for (var i = 0; i < totalParticles; i++) {
+        var spriteMaterial = new THREE.SpriteMaterial({
+            map: particleTexture,
+            useScreenCoordinates: false,
+            color: 0xffffff,
+            transparent: true,
+            blending: THREE.AdditiveBlending
+        });
+
+
+
+
+
+
+
+
+
+        var sprite = new THREE.Sprite(spriteMaterial);
+        sprite.scale.set(32, 32, 1.0); // imageWidth, imageHeight
+        sprite.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+        // for a cube:
+        // sprite.position.multiplyScalar( radiusRange );
+        // for a solid sphere:
+        // sprite.position.setLength( radiusRange * Math.random() );
+        // for a spherical shell:
+        sprite.position.setLength(radiusRange * (Math.random() * 0.1 + 0.9));
+
+        // sprite.color.setRGB( Math.random(),  Math.random(),  Math.random() ); 
+        sprite.material.color.setHSL(Math.random(), 0.4, 0.9);
+
+        // sprite.opacity = 0.80; // translucent particles
+        sprite.material.blending = THREE.AdditiveBlending; // "glowing" particles
+
+        particleGroup.add(sprite);
+        // add variable qualities to arrays, if they need to be accessed later
+        particleAttributes.startPosition.push(sprite.position.clone());
+        particleAttributes.randomness.push(Math.random());
+
+
+
+
+
     }
+    particleGroup.position.y = 00;
+    scene.add(particleGroup);
+}
 
-
-    function Particles()
-{    geometry = new THREE.Geometry();
-
-                for ( i = 0; i < 19000; i ++ ) {
-
-                    var vertex = new THREE.Vector3();
-                    vertex.x = Math.random() * 19000 - 6000;
-                    vertex.y = Math.random() * 8000 - 1000;
-                    vertex.z = Math.random() * 17000 - 4000;
-
-                    geometry.vertices.push( vertex );
-
-                }
-//colors
-                parameters = [
-                    [ [1, .4, .6], 5 ],
-                    [ [1, .3, 0.4], 14 ],
-                    [ [.8, .2, 0.5], 13 ],
-                    [ [.5, .7, 0.3], 16 ],
-                    [ [1, .5, 0.9], 11 ]
-                ];
-
-                for ( i = 0; i < parameters.length; i ++ ) {
-
-                    color = parameters[i][0];
-                    size  = parameters[i][1];
-
-                    materials[i] = new THREE.PointsMaterial( { size: size } );
-
-                    particles = new THREE.Points( geometry, materials[i] );
-
-                    particles.rotation.x = Math.random() * 6;
-                    particles.rotation.y = Math.random() * 6;
-                    particles.rotation.z = Math.random() * 6;
-
-                    scene.add( particles );
-
-                }
-
-
-
-            }
-
-
-
-
+var particleGroup, particleAttributes;
