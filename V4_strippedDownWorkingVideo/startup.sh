69694f10baf14400d79a8d2df5e5a0354b32fd00
Eve Weinberg <eve@neveroddoreven.tv>
2016-03-27 12:35:04 -0400

    onliver version working

--- V4_strippedDownWorkingVideo/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-        export PATH=$PATH:/usr/local/bin
-        if [ $(ps ax | grep -v grep | grep "python -m SimpleHTTPServer 1234" | wc -l) -eq 0 ]
-        then
-                echo "Starting local server at port 1234, cityscape..."
-                cd /Users/Aloni/audicity ; python -m SimpleHTTPServer 1234
-        else
-		echo "already running."
-        fi
